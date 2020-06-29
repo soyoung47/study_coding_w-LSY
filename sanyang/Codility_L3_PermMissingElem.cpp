acf7157b8e8da18017341ddbf4830b8f61684edd
Sanyang-ieee <9639638+seonyoungleee@users.noreply.github.com>
2020-06-29 13:19:11 +0200

    Update and rename Codility_PermMissingElem.cpp to Codility_L3_PermMissingElem.cpp

--- sanyang/Codility_L3_PermMissingElem.cpp
+++ sanyang/Codility_L3_PermMissingElem.cpp
@@ -6,7 +6,6 @@
 int solution(vector<int>& A)
 {
 	int ans = 0;
-	//int maxV = *max_element(A.begin(), A.end()); //max_element returns an iter, so *
 	int maxV = A.size() + 1;
 	int Asum = 0, Rsum = 0;
 	for (int i = 1; i <= maxV; i++)	Asum += i;
