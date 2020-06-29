5158ef55391ad88a0c8de7740dea44c6886538f1
Sanyang-ieee <9639638+seonyoungleee@users.noreply.github.com>
2020-06-29 13:08:37 +0200

    Create Codility_PermMissingElem.cpp

---
+++ sanyang/Codility_PermMissingElem.cpp
@@ -0,0 +1,18 @@
+//Codility_L3_TimeComplexity_PermMissingElem[Painless]
+//https://app.codility.com/programmers/lessons/3-time_complexity/
+
+#include <algorithm>
+
+int solution(vector<int>& A)
+{
+	int ans = 0;
+	//int maxV = *max_element(A.begin(), A.end()); //max_element returns an iter, so *
+	int maxV = A.size() + 1;
+	int Asum = 0, Rsum = 0;
+	for (int i = 1; i <= maxV; i++)	Asum += i;
+	for (int i = 0; i < A.size(); i++)	Rsum += A[i];
+	ans = Asum - Rsum;
+	return ans;
+}
+
+//result: https://app.codility.com/demo/results/training5BGZZ3-32F/ 
