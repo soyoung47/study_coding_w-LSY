e657006f8bfd0e2b82dfcdaa892aaa55216f5f7c
soyoung47 <sy47lee@gmail.com>
2020-07-10 21:30:05 +0900

    Add solution of Codility L6-4

--- soyoung/70. L6-4_Triangle.cpp
+++ soyoung/70. L6-4_Triangle.cpp
@@ -1,4 +1,30 @@
 /*
+    [solution 2]
+    Time Complexity: O(N*log(N))
+    Result:::https://app.codility.com/demo/results/trainingRFPHPV-5DV/
+    Reference:::https://medium.com/@molchevsky/best-solutions-for-codility-lessons-lesson-6-sorting-770f931b65ad
+*/
+#include <algorithm>
+
+int solution(vector<int>& A) {
+
+    //triangular triplet : N >= 3
+    if (A.size() < 3)    return 0;
+
+    sort(A.begin(), A.end());
+
+    for (int i = 0; i < A.size() - 2; i++)
+    {
+        //A[P] + A[Q] > A[R]
+        if (A[i] > A[i + 2] - A[i + 1])
+            return 1;
+    }
+
+    return 0;
+}
+
+/*
+    [solution 1]
     Time Complexity: O(N*log(N))
     Result:::https://app.codility.com/demo/results/training2G87FN-89Q/
 */
