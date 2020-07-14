45fe600158a9f155c01a3439e3238c142bcbe6f0
Sanyang-ieee <9639638+seonyoungleee@users.noreply.github.com>
2020-07-14 12:39:20 +0200

    Create Codility_6-2_MaxProductOfThree.cpp

---
+++ sanyang/Codility_6-2_MaxProductOfThree.cpp
@@ -0,0 +1,19 @@
+//Codility_6-2_Sorting_MaxProductOfThree[Painless]
+//https://app.codility.com/programmers/lessons/6-sorting/max_product_of_three/
+
+#include <algorithm>
+
+int solution(vector<int>& A)
+{
+	sort(A.begin(), A.end());
+	int a = A[A.size()-1] * A[A.size()-2] * A[A.size()-3];
+	int b = A[0] * A[1] * A[A.size() - 1];
+	return (a > b) ? a : b;
+}
+
+//result(100% O(N * log(N))
+//https://app.codility.com/demo/results/trainingCWFQ9R-XVF/
+
+//ref:
+//https://killong.blogspot.com/2019/03/codility-lesson6-maxproductofthree.html
+//https://moonscode.tistory.com/128
