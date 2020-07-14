07c4688ebeb800a957e4b70473b24885b9e69866
soyoung47 <sy47lee@gmail.com>
2020-07-14 22:13:19 +0900

    Add solution of Codility L6-2

--- soyoung/68. L6-2_Max Product Of Three.cpp
+++ soyoung/68. L6-2_Max Product Of Three.cpp
@@ -1,4 +1,25 @@
 /*
+    [solution 2]
+    Time Complexity: O(N * log(N))
+    Result:::https://app.codility.com/demo/results/trainingMT525W-C24/
+*/
+#include <algorithm>
+
+int solution(vector<int>& A) {
+
+    int product1 = 0; // -,-,+
+    int product2 = 0; // +,+,+
+    int size = A.size();
+
+    sort(A.begin(), A.end());
+    product1 = A[0] * A[1] * A[size-1];
+    product2 = A[size - 1] * A[size - 2] * A[size - 3];
+
+    return (product1 > product2) ? product1 : product2;
+}
+
+/*
+    [solution 1]
     Time Complexity: O(N * log(N))
     Result:::https://app.codility.com/demo/results/trainingGBNYBN-UJJ/
 */
