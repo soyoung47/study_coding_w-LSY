4857effd969852f9b99c587efcac6d5850d3963a
soyoung47 <sy47lee@gmail.com>
2020-07-23 00:41:31 +0900

    Add solution of Codility L8-1

--- soyoung/75. L8-1_Dominator.cpp
+++ soyoung/75. L8-1_Dominator.cpp
@@ -1,4 +1,40 @@
 /*
+    [solution 2]
+    Time Complexity: O(N*log(N)) or O(N)
+    Result:::https://app.codility.com/demo/results/trainingY2GWXT-V8C/
+    Reference:::https://codility.com/media/train/6-Leader.pdf
+*/
+#include <algorithm>
+
+int solution(vector<int>& A) {
+
+    int leader = 0;
+    int size = A.size();
+    int cnt = 0;
+    int index = 0;
+
+    if (size == 0) return -1;
+
+    vector<int> B = A;
+    sort(B.begin(), B.end());
+    leader = B[size / 2];
+
+    for (int i = 0; i < size; i++)
+    {
+        if (A[i] == leader)
+        {
+            cnt++;
+            index = i;
+        }
+    }
+
+    if (cnt < (size / 2) + 1)    return -1;
+
+    return index;
+}
+
+/*
+    [solution 1]
     Time Complexity: O(N*log(N)) or O(N)
     Result:::https://app.codility.com/demo/results/training9ARFJ5-YW8/
 */
