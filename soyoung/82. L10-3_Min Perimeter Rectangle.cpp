4cfc7c30fabc9adfefe4d88c1d1fa4d49573223a
soyoung47 <0407sally@naver.com>
2020-07-27 00:34:14 +0900

    Upload Codility L10-3

---
+++ soyoung/82. L10-3_Min Perimeter Rectangle.cpp
@@ -0,0 +1,22 @@
+///*
+//    Time Complexity: O(sqrt(N))
+//    Result:::https://app.codility.com/demo/results/training74RU83-44G/
+//*/
+//#include <math.h>
+//
+//int solution(int N) {
+//
+//    int perimeter = 0;
+//    int min = 2 * (1 + N);
+//
+//    for (int i = 2; i <= sqrt(N); i++)
+//    {
+//        if (N % i == 0)
+//        {
+//            perimeter = 2 * (i + N/i);
+//            min = (min < perimeter) ? min : perimeter;
+//        }
+//    }
+//
+//    return min;
+//}
