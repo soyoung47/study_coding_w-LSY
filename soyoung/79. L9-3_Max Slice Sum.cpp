9237c98dcb1b46bb28ca5dfb7069fd2a9da404a9
soyoung47 <0407sally@naver.com>
2020-07-28 19:53:41 +0900

    Add solution of Codility L9-3

--- soyoung/79. L9-3_Max Slice Sum.cpp
+++ soyoung/79. L9-3_Max Slice Sum.cpp
@@ -1,4 +1,26 @@
 /*
+    [solution 2]
+    Time Complexity: O(N)
+    Result:::https://app.codility.com/demo/results/training2BFGM9-ECB/
+    Reference:::
+*/
+int solution(vector<int>& A) {
+
+    int size = A.size();
+    int maxEnd = -1000000;
+    int sum = 0;
+
+    for (int i = 0; i < size; i++)
+    {
+        maxEnd = (maxEnd > sum + A[i]) ? maxEnd : sum + A[i];
+        sum = (0 > sum + A[i]) ? 0 : sum + A[i];
+    }
+
+    return maxEnd;
+}
+
+/*
+    [solution 1]
     Time Complexity: O(N)
     Result:::https://app.codility.com/demo/results/trainingXF6PKU-ZMH/
     Reference:::https://codility.com/media/train/7-MaxSlice.pdf
