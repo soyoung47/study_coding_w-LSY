804f6b2a44ee71efd033b3b50638f17108b2fb5c
soyoung47 <0407sally@naver.com>
2020-08-06 20:37:00 +0900

    Add solution of Codility L10-4

--- soyoung/83. L10-4_Peaks.cpp
+++ soyoung/83. L10-4_Peaks.cpp
@@ -1,4 +1,47 @@
 /*
+    [solution 2]
+    Time Complexity: O(N * log(log(N)))
+    Result:::https://app.codility.com/demo/results/trainingZXVZVE-HRK/
+    Reference::: http://dal4segno.github.io/algorithm/codility-10-primes-and-composite-numbers/
+*/
+int solution(vector<int>& A) {
+
+    vector<int> peak;
+    int size = A.size();
+
+    if (size < 3)  return 0;
+
+    //Find Peaks
+    for (int p = 1; p < size - 1; p++)
+    {
+        if (A[p] > A[p - 1] && A[p] > A[p + 1])
+            peak.push_back(p);
+    }
+
+    //Find Maximum number of blocks
+    int peak_size = peak.size();
+    if (peak_size < 2)   return peak_size;
+
+    for (int k = peak_size; k > 0; k--)
+    {
+        if (size % k != 0) continue;
+
+        int div_ind = size / k;
+        int cnt = 0;
+        for (int i = 0; i < peak_size; i++) //-> for(int p : peak), peak[i] -> p
+        {
+            if (peak[i] >= cnt * div_ind && peak[i] < (cnt + 1) * div_ind)
+            {
+                cnt++;
+            }
+        }
+
+        if (cnt == k)   return cnt;
+    }
+}
+
+/*
+    [solution 1]
     Time Complexity: O(N * log(log(N)))
     Result:::https://app.codility.com/demo/results/training67H57R-MPD/
     Reference::: https://blog.naver.com/PostView.nhn?blogId=yeokyeong46&logNo=221600296267&parentCategoryNo=&categoryNo=7&viewDate=&isShowPopularPosts=false&from=postView
@@ -57,3 +100,9 @@ int solution(vector<int>& A) {
 
     return max_div;
 }
+
+
+//{ 1, 4, 3, 4, 3, 2, 1, 2, 3, 4, 6, 2 };   //3
+//{ 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0 };   //1    
+//{ 1, 2, 3, 4, 3, 4, 1, 2, 3, 4, 6, 2 };   //2
+//{ 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0 };   //4
