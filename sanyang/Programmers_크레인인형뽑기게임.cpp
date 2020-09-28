7e10ab6496c71c78f56eebaf0cb8d6c46e315d85
Sanyang-ieee <9639638+seonyoungleee@users.noreply.github.com>
2020-09-28 12:51:15 +0200

    Update Programmers_크레인인형뽑기게임.cpp

--- sanyang/Programmers_크레인인형뽑기게임.cpp
+++ sanyang/Programmers_크레인인형뽑기게임.cpp
@@ -26,10 +26,3 @@ int solution(vector<vector<int>> board, vector<int> moves) {
     }
     return answer;
 }
-
-int main() {
-    vector<vector<int>> board = { {0,0,0,0,0} ,{0,0,1,0,3},{0,2,5,0,1},{4,2,4,4,2},{3,5,1,3,1} };
-    vector<int> moves = { 1,5,3,5,1,2,1,4 };
-    solution(board, moves);
-    return 0;
-}
