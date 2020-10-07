0f89d1f431e276a93448cd3f0ac05c43352e4df0
soyoung47 <sy47lee@gmail.com>
2020-10-07 23:16:17 +0900

    Add notes Programmers L4_동굴 탐험

--- soyoung/131. L4_동굴 탐험.cpp
+++ soyoung/131. L4_동굴 탐험.cpp
@@ -11,6 +11,7 @@ void dfs(int v)
 {
     if (visit[v])    return;
 
+    //방 번호 v 전에 방문해야 하는 방(prior[v])을 방문 안했으면, hang(대기)에 넣기
     if (!visit[prior[v]])
     {
         hang[prior[v]] = v;
@@ -18,19 +19,21 @@ void dfs(int v)
     }
 
     visit[v] = true;
-    dfs(hang[v]);
+    dfs(hang[v]);   //대기하는 것이 있다면 dfs 진행
     for (int i = 0; i < edge[v].size(); i++)
         dfs(edge[v][i]);
 }
 
 bool solution(int n, vector<vector<int>> path, vector<vector<int>> order) {
 
+    //양방향 그래프
     for (int i = 0; i < path.size(); i++)
     {
         edge[path[i][0]].push_back(path[i][1]);
         edge[path[i][1]].push_back(path[i][0]);
     }
 
+    //A→B(prior[B] = A): 방 번호 B 전에 방문해야 하는 방 번호 A 저장
     for (int i = 0; i < order.size(); i++)
         prior[order[i][1]] = order[i][0];
 
