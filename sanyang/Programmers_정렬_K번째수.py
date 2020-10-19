5890afe658ef8311eeac764039979670def87e95
Sanyang-ieee <9639638+seonyoungleee@users.noreply.github.com>
2020-10-19 16:45:58 +0200

    Create Programmers_정렬_K번째수.py

---
+++ sanyang/Programmers_정렬_K번째수.py
@@ -0,0 +1,10 @@
+//Programmers_정렬_K번째수.py
+
+def solution(array, commands):
+    size = len(commands)
+    answer = [0] * len(commands)
+    for i in range(len(commands)):
+        tmpArr = array[commands[i][0]-1:commands[i][1]]
+        tmpArr.sort()
+        answer[i] = tmpArr[commands[i][2]-1]
+    return answer
