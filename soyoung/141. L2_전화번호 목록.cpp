107f3995617f856190f552f396bd941160070949
soyoung47 <sy47lee@gmail.com>
2020-10-22 00:40:50 +0900

    Add Solution Programmers L2_전화번호 목록

--- soyoung/141. L2_전화번호 목록.cpp
+++ soyoung/141. L2_전화번호 목록.cpp
@@ -1,5 +1,39 @@
 //https://programmers.co.kr/learn/courses/30/lessons/42577
 
+/***Solution 2: 해시***/
+
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <map>
+using namespace std;
+
+bool solution(vector<string> phone_book) {
+    bool answer = true;
+    int size = phone_book.size();
+    unordered_map<string, int> m;
+    map<int, int> len_m;
+
+    for (string phone : phone_book) m[phone] = 1;
+
+    for (auto iter : m)
+    {
+        int len = iter.first.size();
+
+        for (string phone : phone_book)
+        {
+            if (len > phone.size())  continue;
+            if (iter.first == phone) continue;
+            if (m[phone.substr(0, len)] == 1) return false;
+        }
+    }
+
+    return answer;
+}
+
+
+/***Solution 1: 정렬***/
+
 #include <string>
 #include <vector>
 #include <algorithm>
@@ -19,7 +53,7 @@ bool solution(vector<string> phone_book) {
     int size = phone_book.size();
     sort(phone_book.begin(), phone_book.end(), comp);
 
-    for (int i = 0; i < size-1; i++)
+    for (int i = 0; i < size - 1; i++)
     {
         int len = phone_book[i].size();
 
