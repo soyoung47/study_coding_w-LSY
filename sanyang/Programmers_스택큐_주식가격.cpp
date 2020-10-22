cc98147f0368de41916045157ac08f3d9774beb7
Sanyang-ieee <9639638+seonyoungleee@users.noreply.github.com>
2020-10-22 16:07:56 +0200

    Create Programmers_스택큐_주식가격.cpp

---
+++ sanyang/Programmers_스택큐_주식가격.cpp
@@ -0,0 +1,31 @@
+
+#include <string>
+#include <vector>
+#include <stack>
+using namespace std;
+
+vector<int> solution(vector<int> prices) {
+    int size = prices.size();
+    vector<int> answer(size);
+    stack<int> st;
+    for (int i=0; i<size; i++){
+        while(!st.empty() && prices[st.top()]>prices[i]){
+            answer[st.top()]=i-st.top();
+            st.pop();
+        }
+        st.emplace(i);
+    }
+    while(!st.empty()){
+        answer[st.top()]=size-st.top()-1;
+        st.pop();
+    }
+    return answer;
+}
+
+int main(){
+    vector<int> prices={1,2,3,2,3};
+    solution(prices);
+    return 0;
+}
+
+//https://mungto.tistory.com/6
