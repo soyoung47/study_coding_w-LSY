c1f232dacb36fbf2af149bf84a3f2ac2da375c5c
soyoung47 <sy47lee@gmail.com>
2021-03-05 01:06:25 +0900

    Add Solution BOJ 2565_전깃줄

--- soyoung/215. 2565_전깃줄.cpp
+++ soyoung/215. 2565_전깃줄.cpp
@@ -1,5 +1,7 @@
 //https://www.acmicpc.net/problem/2565
-//2016KB, 0ms
+
+/***Solution 2: DP***/
+//2016kb, 0ms
 
 #include <iostream>
 #include <algorithm>
@@ -12,9 +14,51 @@ int main()
 	int n, to, from, ind, pos, ans = 0;
 	cin >> n;
 
-	vector<pair<int,int>> pole;	//전봇대 연결 정보(A 전봇대, B 전봇대)
-	vector<int> select;	//선택된 B 전봇대 위치 번호
-	
+	vector<pair<int, int>> pole;	//전봇대 연결 정보(a 전봇대, b 전봇대)
+	vector<int> dp(n, 0);
+
+	for (int i = 0; i < n; i++)
+	{
+		cin >> from >> to;
+		pole.emplace_back(from, to);
+	}
+
+	sort(pole.begin(), pole.end());
+
+	for (int i = 0; i < n; i++)
+	{
+		dp[i] = 1;
+
+		for (int j = 0; j < i; j++)
+		{
+			if (pole[i].second > pole[j].second)
+				dp[i] = max(dp[i], dp[j] + 1);
+		}
+
+		ans = max(ans, dp[i]);
+	}
+
+	cout << n - ans;
+}
+
+
+/***Solution 1: lower_bound***/
+//2016kb, 0ms
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <utility>
+using namespace std;
+
+int main()
+{
+	int n, to, from, ind, pos, ans = 0;
+	cin >> n;
+
+	vector<pair<int, int>> pole;	//전봇대 연결 정보(a 전봇대, b 전봇대)
+	vector<int> select;	//선택된 b 전봇대 위치 번호
+
 	for (int i = 0; i < n; i++)
 	{
 		cin >> from >> to;
