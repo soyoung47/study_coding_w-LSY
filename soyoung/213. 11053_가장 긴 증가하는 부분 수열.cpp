73c14bb1c7b763a67e52ae5623daeef8735e6886
soyoung47 <sy47lee@gmail.com>
2021-03-04 23:48:28 +0900

    Add Solution BOJ 11053_가장 긴 증가하는 부분 수열

--- soyoung/213. 11053_가장 긴 증가하는 부분 수열.cpp
+++ soyoung/213. 11053_가장 긴 증가하는 부분 수열.cpp
@@ -1,5 +1,7 @@
 //https://www.acmicpc.net/problem/11053
-//2016KB, 0ms
+
+/***Solution 1: lower_bound***/
+//2016kb, 0ms
 
 #include <iostream>
 #include <vector>
@@ -9,7 +11,7 @@ using namespace std;
 int main()
 {
 	int n, elemt, ind;
-	vector<int> A;	//선택된 elemt(원소)의 index값 저장됨.
+	vector<int> a;	//선택된 elemt(원소)의 index값 저장됨.
 
 	cin >> n;
 
@@ -17,13 +19,46 @@ int main()
 	{
 		cin >> elemt;
 
-		ind = lower_bound(A.begin(), A.end(), elemt) - A.begin();	//수열A 중 원소값보다 큰 가장 작은 값의 위치반환
-	
-		if (ind == A.size())
-			A.push_back(elemt);
+		ind = lower_bound(a.begin(), a.end(), elemt) - a.begin();	//수열a 중 원소값보다 큰 가장 작은 값의 위치반환
+
+		if (ind == a.size())
+			a.push_back(elemt);
 		else
-			A[ind] = elemt;
+			a[ind] = elemt;
 	}
 
-	cout << A.size();
+	cout << a.size();
+}
+
+/***Solution 2: DP***/
+//2016kb, 0ms
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+	int n, ans = 0;
+	cin >> n;
+
+	vector<int> a(n, 0);
+	vector<int> dp(n, 0);
+
+	for (int i = 0; i < n; i++)
+		cin >> a[i];
+
+	for (int i = 0; i < n; i++)
+	{
+		dp[i] = 1;
+		for (int j = 0; j < i; j++)
+		{
+			if (a[i] > a[j])
+				dp[i] = max(dp[i], dp[j] + 1);
+		}
+		ans = max(ans, dp[i]);
+	}
+
+	cout << ans;
 }
